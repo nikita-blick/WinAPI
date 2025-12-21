a735f8561a82e5570db072d2775d1362b0d8a08f
Nikita <Nikita@DESKTOP-13IEN5T>
2025-12-21 15:22:07 +0500

    Colors DONE

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -27,6 +27,16 @@ CONST INT g_i_WINDOW_HEIGHT = g_i_DISPLAY_HEIGHT + g_i_START_Y + (g_i_BUTTON_SIZ
 
 CONST CHAR g_OPERATIONS[] = "+-*/";
 
+CONST INT g_i_WINDOW_COLOR = 0;
+CONST INT g_i_DISPLAY_COLOR = 1;
+CONST INT g_i_FONT_COLOR = 2;
+CONST COLORREF g_clr_COLORS[][3] =
+{
+	{RGB(0,0,150), RGB(0,0,100), RGB(255,0,0)},
+	{ RGB(150,150,150), RGB(50,50,50), RGB(0,255,0)},
+};
+CONST CHAR* g_sz_SKIN[] = { "square_blue", "metal_mixtral" };
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 VOID SetSkin(HWND hwnd, CONST CHAR skin[]);
 
@@ -95,6 +105,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	static int skinID = 0;
 	switch (uMsg)
 	{
 	case WM_CREATE:
@@ -240,13 +251,14 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_CTLCOLOREDIT:
 	{
 		HDC hdc = (HDC)wParam;     // C сообщение WM_CTLCOLOREDIT in 'wParam' принимается HDC элемента EditControl
-		SetBkMode(hdc, OPAQUE);    // Делаем фон hEdit непрозрачным
-		SetBkColor(hdc, RGB(0, 0, 250));
-		SetTextColor(hdc, RGB(255, 0, 0));
-		HBRUSH hBrush = CreateSolidBrush(RGB(0, 0, 150));
+		//SetBkMode(hdc, OPAQUE);    // Делаем фон hEdit непрозрачным
+		SetBkColor(hdc, g_clr_COLORS[skinID][g_i_DISPLAY_COLOR]);
+		SetTextColor(hdc, g_clr_COLORS[skinID][g_i_FONT_COLOR]);
+		HBRUSH hBrush = CreateSolidBrush(g_clr_COLORS[skinID][g_i_WINDOW_COLOR]);
 		SetClassLongPtr(hwnd, GCLP_HBRBACKGROUND, (LONG_PTR)hBrush);
 		SendMessage(hwnd, WM_ERASEBKGND, wParam, 0);
-		return (LRESULT)hBrush;
+		DeleteObject(hBrush);
+		//return (LRESULT)hBrush;
 	}
 	case WM_COMMAND:
 	{
@@ -429,7 +441,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		HMENU cmMain = CreatePopupMenu();
 		AppendMenu(cmMain, MF_STRING, IDM_SQUARE_BLUE, "Square blue");
-		AppendMenu(cmMain, MF_STRING, IDM_METAL_MIXTRAL, "Metal mistral");
+		AppendMenu(cmMain, MF_STRING, IDM_METAL_MIXTRAL, "Metal mixtral");
 		AppendMenu(cmMain, MF_SEPARATOR, NULL, NULL);
 		AppendMenu(cmMain, MF_STRING, IDM_EXIT, "Exit");
 
@@ -444,10 +456,15 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		);
 		switch (selected_item)
 		{
-		case IDM_SQUARE_BLUE:	SetSkin(hwnd, "square_blue");	break;
-		case IDM_METAL_MIXTRAL:	SetSkin(hwnd, "metal_mistral");	break;
+		case IDM_SQUARE_BLUE:	skinID = 0;	break;
+		case IDM_METAL_MIXTRAL:	skinID = 1;	break;
 		case IDM_EXIT:			SendMessage(hwnd, WM_CLOSE, 0, 0); break;
 		}
+		/*HDC hdc = GetDC(hwnd);
+		SendMessage(hwnd, WM_PAINT, (WPARAM)hdc, (LPARAM)hwnd);
+		ReleaseDC(hwnd, hdc);*/
+		InvalidateRect(hwnd, 0, TRUE);
+		SetSkin(hwnd, g_sz_SKIN[skinID]);
 		DestroyMenu(cmMain);
 	}
 	break;
